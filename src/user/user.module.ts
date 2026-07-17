77150d61541f8e16a5f6da1cb5fb3dc6adaea64f
not-iron-yury <not.iron.yuriy@inbox.ru>
2026-07-17 17:21:30 +0300

    refactor(user): исправление импортов в UserModule

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
+import { UserProfileRepository, UserRepository } from 'src/user/repositories';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserRepository, UserProfileRepository],
+  exports: [UserService, UserRepository, UserProfileRepository],
 })
 export class UserModule {}
