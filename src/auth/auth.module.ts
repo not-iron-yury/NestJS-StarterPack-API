e39efbb5aa710d25f673c29f6a304d81d9d9b644
not-iron-yury <not.iron.yuriy@inbox.ru>
2026-07-18 17:26:56 +0300

    feat(auth): подключение сервисов в AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,25 +3,37 @@ import { Module } from '@nestjs/common';
 import { CommonModule } from 'src/common';
 import { AppConfigModule, AppConfigProvider } from 'src/config';
 
+import { JwtService } from '@nestjs/jwt';
+import { PasswordService } from 'src/common/services';
+import { UserRepository } from 'src/user/repositories';
+import { UserModule } from 'src/user/user.module';
+import { UserService } from 'src/user/user.service';
 import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
 import {
   EmailThrottleRepository,
   EmailTokenRepository,
   IpThrottleRepository,
   RefreshTokenRepository,
 } from './repositories';
+import { RateLimitService, TokenService } from './services';
 
 @Module({
-  imports: [AppConfigModule, CommonModule],
+  imports: [AppConfigModule, CommonModule, UserModule],
   controllers: [AuthController],
   providers: [
-    AuthService,
-    EmailThrottleRepository,
+    AppConfigProvider,
+
+    UserService,
+    PasswordService,
+    RateLimitService,
+    TokenService,
+    JwtService,
+
+    UserRepository,
+    RefreshTokenRepository,
     EmailTokenRepository,
     IpThrottleRepository,
-    RefreshTokenRepository,
-    AppConfigProvider,
+    EmailThrottleRepository,
   ],
 })
 export class AuthModule {}
---
+++ src/auth/services/index.ts
@@ -0,0 +1,4 @@
+export * from './auth.service';
+export * from './cookie.service';
+export * from './rate-limit.service';
+export * from './token.service';
