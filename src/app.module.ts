f9e531f02d29a769df97a8b573d76f230121e2e9
not-iron-yury <not.iron.yuriy@inbox.ru>
2026-07-18 20:21:16 +0300

    fix: удалены ненужные AppController и AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from 'src/config/app-config.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -15,7 +13,5 @@ import { UserModule } from './user/user.module';
     PrismaModule,
     AppConfigModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
